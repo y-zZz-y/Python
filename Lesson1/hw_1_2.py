b42053e483c26d9c05ba9c2e5fd1cc1376c17194
yzZzy <0@yzzzy.ru>
2020-11-02 15:47:57 +0500

    Домашняя работа

--- Lesson1/hw_1_2.py
+++ Lesson1/hw_1_2.py
@@ -1,4 +1,7 @@
-print('*'*10, 'Задание 2', '*'*10, end='\n\n')
+# Пользователь вводит время в секундах.
+# Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
+# Используйте форматирование строк.
+
 
 userTime = int(input('Введите время в секундах: '))
 
@@ -6,13 +9,7 @@ userTimeHours = userTime//3600
 userTimeMinutes = (userTime - (userTime//3600)*3600)//60
 userTimeSeconds = userTime - userTimeHours*3600 - userTimeMinutes*60
 
-if userTimeHours < 10:
-    userTimeHours = '{}{}'.format('0', userTimeHours)
-if userTimeMinutes < 10:
-    userTimeMinutes = '{}{}'.format('0', userTimeMinutes)
-if userTimeSeconds < 10:
-    userTimeSeconds = '{}{}'.format('0', userTimeSeconds)
 
-userTimeLine = '{}:{}:{}'.format(userTimeHours, userTimeMinutes, userTimeSeconds)
+userTimeLine = '{0:02d}:{1:02d}:{2:02d}'.format(userTimeHours, userTimeMinutes, userTimeSeconds)
 
 print(userTime, 'в формате чч:мм:cc', userTimeLine)
